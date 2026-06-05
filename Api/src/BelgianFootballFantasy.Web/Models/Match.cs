8e6cb173e54e7a06e639207757068ab39bfecdfc
PirvanLiviu <liviu.pirvan@student.howest.be>
2026-06-05 22:57:40 +0200

    Typo resolved

--- Api/src/BelgianFootballFantasy.Web/Models/Match.cs
+++ Api/src/BelgianFootballFantasy.Web/Models/Match.cs
@@ -2,11 +2,11 @@ namespace Api.Models;
 
 public class Match
 {
-    public Int Id { get; set; }
-    public Int HomeTeamId { get; set; }
+    public int Id { get; set; }
+    public int HomeTeamId { get; set; }
     public Team HomeTeam { get; set; } = null!;
 
-    public Int AwayTeamId { get; set; }
+    public int AwayTeamId { get; set; }
     public Team AwayTeam { get; set; } = null!;
 
     public DateTime Date { get; set; }
